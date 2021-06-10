4a9f74d586b0be7693ca155c18a8e3df190ae54d
Richard Zhang <richard_zhang88@htomail.com>
2021-06-10 11:40:23 -0400

    commit third try

--- XBDAppPortalWebFormAppPrj/XBDAppPortalList.aspx.cs
+++ XBDAppPortalWebFormAppPrj/XBDAppPortalList.aspx.cs
@@ -67,7 +67,7 @@ namespace XBDAppPortalWebFormAppPrj
             }
         }
 
-        //this moethod will be removed later. check in test again. one more try.
+        //this moethod will be removed later. check in test again. one more try. second try, the third try.
         protected void ddlAppName_SelectedIndexChanged(object sender, EventArgs e)
         {
             //var selectAppName = this.ddlAppName.SelectedValue;
